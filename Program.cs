da0ac120aadec9413a4a95f45ed7658726ed6b47
Sebastian Falborg <sebastianfalborg@gmail.com>
2021-12-13 00:15:08 +0100

    Change to use CloudflareBlazorTrialApi, add appsettings.

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+string baseAddress = builder.Configuration["baseAddress"] ?? string.Empty;
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
 
 await builder.Build().RunAsync();
